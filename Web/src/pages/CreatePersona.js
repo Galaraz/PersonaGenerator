c07dd62fcb347735f2fc620c9cb77e8c459f0824
Galaraz <jvictor.si.2013@gmail.com>
2021-04-27 11:39:19 -0400

    fix

--- Web/src/pages/CreatePersona.js
+++ Web/src/pages/CreatePersona.js
@@ -1,14 +1,42 @@
-import React from 'react';
-
-
-export default function CreatePersona() {
-  return(
-<>
-<label>
-    Name:
-    <input type="text" name="name" />
-  </label>
-<button>creacte</button>
-  </>  
-  );
-}
+  // REF - https://www.digitalocean.com/community/tutorials/react-axios-react-pt
+  import React from 'react';
+  import axios from 'axios';
+  
+  export default class CreatePersona extends React.Component {
+    state = {
+      name: '',
+    }
+  
+    handleChange = event => {
+      this.setState({ name: event.target.value });
+    }
+  
+    handleSubmit = event => {
+      event.preventDefault();
+  
+      const user = {
+        name: this.state.name
+      };
+  
+      
+      axios.post(`http://localhost:3000/persona/`, { user })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+    }
+  
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Person Name:
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      )
+    }
+  }
